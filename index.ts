03d379505df8735dc5b24c9e423838ea295f1cf0
Nathanim1919 <nathanim2tadele@gmail.com>
2024-11-05 20:45:31 +0300

    add memory for the agent

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import 'dotenv/config'
+import { runLM } from './src/llm'
+import dotenv from 'dotenv'
+import { addMessages, getMessages } from './src/memory'
+
+dotenv.config()
 
 const userMessage = process.argv[2]
 
@@ -6,3 +11,12 @@ if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+
+await addMessages([{ content: userMessage, role: 'user' }])
+const messages = await getMessages()
+
+const response = await runLM({
+  messages,
+})
+
+console.log(response)
